4146c9f220801b0e33aeb7b8139dd2b0212f3986
cfcalder <ccalderonr@grupoasd.com.co>
2022-10-12 15:17:25 -0500

    test jonatan

--- infrastructure/entry-points/api-rest/src/main/java/co/com/grupoasd/api/ApiRest.java
+++ infrastructure/entry-points/api-rest/src/main/java/co/com/grupoasd/api/ApiRest.java
@@ -6,15 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
 public class ApiRest {
 //    private final MyUseCase useCase;
 
 
-    @GetMapping(path = "/path")
+    @GetMapping
     public String commandName() {
 //      return useCase.doAction();
-        return "Hello World";
+        return "Hello Jonatan";
     }
 }
